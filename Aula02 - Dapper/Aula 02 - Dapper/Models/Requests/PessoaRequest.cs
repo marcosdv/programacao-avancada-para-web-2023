ffac173379bfeffdb4d756e82b821f3e86f3410b
Marcos Dias Vendramini <marcosdiasvendramini@yahoo.com.br>
2023-08-30 22:14:10 -0300

    Delete Aula02 - Dapper directory

--- Aula02 - Dapper/Aula 02 - Dapper/Models/Requests/PessoaRequest.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Aula_02___Dapper.Models.Requests;
-
-public class PessoaRequest
-{
-    public int? Id { get; set; }
-    public string Nome { get; set; }
-}
--- Aula02 - Dapper/Aula 02 - Dapper/Models/Requests/TelefoneRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Aula_02___Dapper.Models.Requests;
-
-public class TelefoneRequest
-{
-    public int? Id { get; set; }
-    public string Numero { get; set; }
-    public int Operadora { get; set; }
-    public int Pessoa { get; set; }
-}
