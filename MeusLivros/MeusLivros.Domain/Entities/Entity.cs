d4f1ee1bd324a537785da79ed142153a1eb5b70c
Marcos Dias Vendramini <marcosdiasvendramini@yahoo.com.br>
2023-09-06 22:15:01 -0300

    Aula05 - 06-09-2023

---
+++ MeusLivros/MeusLivros.Domain/Entities/Entity.cs
@@ -0,0 +1,11 @@
+﻿namespace MeusLivros.Domain.Entities;
+
+public class Entity : IEquatable<Entity>
+{
+    public int Id { get; set; }
+
+    public bool Equals(Entity? other)
+    {
+        return this.Id == other?.Id;
+    }
+}
