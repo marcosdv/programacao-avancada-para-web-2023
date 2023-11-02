a6f553cec20b03eeb2601b8044809aa3f9883a0e
Marcos Dias Vendramini <marcosdiasvendramini@yahoo.com.br>
2023-11-01 21:56:51 -0300

    Delete MeusLivros directory

--- MeusLivros/MeusLivros.Domain/Handlers/Interfaces/IHandler.cs
+++
@@ -1,8 +0,0 @@
-﻿using MeusLivros.Domain.Commands.Interfaces;
-
-namespace MeusLivros.Domain.Handlers.Interfaces;
-
-public interface IHandler<T> where T : ICommand
-{
-    ICommandResult Handle(T command);
-}
